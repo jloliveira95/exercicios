d9809b60bc38e633b018b4bf82ac5a71dc9ccba8
jloliveira95 <jl.oliveira95@gmail.com>
2024-12-05 08:34:47 -0300

    Exercícios de loops

--- src/estrutura_decisão/exercicioIfElseEncadeado.java
+++ src/estrutura_decisão/exercicioIfElseEncadeado.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package estrutura_decisão;
 
 import java.util.Scanner;
 
-/**
- *
- * @author User
- */
+
 public class exercicioIfElseEncadeado {
     
     public static void main(String[] args) {
